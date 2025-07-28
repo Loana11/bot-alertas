d0312c1d1b367ffd705052b31fa0f4c4b74389de
Loana11 <mambrudloana@gmail.com>
2025-07-28 14:23:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+    
+@app.route("/check")
+def check_prices():
+    from stock_monitor import check_all_stocks
+    check_all_stocks()
+    return "Precios revisados"
